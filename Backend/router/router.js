208b037c769ac2e810a2d2751e4d87180ebe16a2
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-03-15 16:08:45 +0530

    Update router.js

--- Backend/router/router.js
+++ Backend/router/router.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const upload = require('../multer/upload')
 const { postBlog, getBlogs } = require('../controller/blogControle')
 
-router.post('/post/v1/createblog' , upload.single('file') , postBlog)
+router.post('/post/v1/createblog' , postBlog)
 
 router.get('/get/v1/blogs' , getBlogs)
 
